6d157c31caa5a83714e708d5419165c1fb078b76
Hyun <hnjogo@gmail.com>
2025-08-12 11:43:44 +0900

    메인 게임 내용 수정

--- Task2/MainGame.cpp
+++ Task2/MainGame.cpp
@@ -1,74 +1,72 @@
-﻿#include "MainGame.h"
+﻿#include<iostream>
+#include<string>
+#include "MainGame.h"
 #include "Player.h"
 #include "Util.h"
-#include<iostream>
-#include<string>
 #include "Enums.h"
 #include "Skill.h"
+#include "Monster.h"
+#include "Warrior.h"
+#include "Archer.h"
+#include "Thief.h"
+#include "Magician.h"
 
 MainGame::MainGame()
 	:MyPlayer(nullptr)
 {
-	tester = new Player(1000,1000,40,0,100,10,"Tester");
-	tester->AddSkill(SkillIdx::BaseAttack, { 2.0,0 });
+	MyMonster = new Monster(10,100,30,10,10,10,"웨어울프");
+	MyMonster->AddSkill(SkillIdx::BaseAttack, { 2.0,0 });
 }
 
 MainGame::~MainGame()
 {
 	SafeDelete(MyPlayer);
-	SafeDelete(tester);
+	SafeDelete(MyMonster);
 }
 
 void MainGame::GameSetting()
 {
-	int hp = 0, mp = 0;
-	while (hp < 60 || mp < 60)
-	{
-		cout << "HP와 MP를 입력해주세요:";
-		cin >> hp >> mp;
-		if (hp < 60 || mp < 60)
-		{
-			cout << "HP나 MP의 값이 너무 작습니다. 다시 입력해주세요." << '\n';
-		}
-	}
-
-	int attack = 0, defense = 0;
-	while (attack < 30 || defense < 30)
-	{
-		cout << "공격력과 방어력을 입력해주세요:";
-		cin >> attack >> defense;
-		if (attack < 30 || defense < 30)
-		{
-			cout << "공격력이나 방어력의 값이 너무 작습니다. 다시 입력해주세요." << '\n';
-		}
-	}
-
-	int accuracy = 0, speed = 0;
-	while (accuracy < 30 || speed < 30)
-	{
-		cout << "정확도와 속도를 입력해주세요:";
-		cin >> accuracy >> speed;
-		if (accuracy < 30 || speed < 30)
-		{
-			cout << "정확도나 속도의 값이 너무 작습니다. 다시 입력해주세요." << '\n';
-		}
-	}
-	
-	cout << "마지막으로 당신의 이름을 입력해주세요:";
+	string jobs[] = { "전사", "마법사", "도적", "궁수" };
+	int job_choice = 0;
 	string name;
+
+	cout << "* 이름을 입력해주세요: ";
 	cin >> name;
-	MyPlayer = new Player(hp, mp, attack, defense,accuracy,speed, name);
+
+	cout << "<전직 시스템>" << endl;
+	cout << name << "님, 환영합니다!" << endl;
+	cout << "* 원하시는 직업을 선택해주세요." << endl;
+
+	for (int i = 0; i < 4; i++) {
+		cout << (i + 1) << ". " << jobs[i] << endl;
+	}
+
+	while (job_choice <= 0 || job_choice > 4)
+	{
+		cin >> job_choice;
+		switch (job_choice) 
+		{
+		case 1:
+			MyPlayer = new Warrior(100, 100, 40, 50, 5, 5, name);
+			break;
+		case 2:
+			MyPlayer = new Magician(50, 200, 30, 40, 6, 6, name);
+			break;
+		case 3:
+			MyPlayer = new Thief(60, 100, 60, 20, 10, 10, name);
+			break;
+		case 4:
+			MyPlayer = new Archer(60, 100, 50, 30, 10, 10, name);
+			break;
+		default:
+			cout << "잘못된 입력입니다." << endl;
+			break;
+		}
+	}
 
 	MyPlayer->AddSkill(SkillIdx::BaseAttack, { 2,50 });
 	MyPlayer->AddSkill(SkillIdx::LethalAttack, { 3,0 });
 
-	MyPlayer->GetItem(ItemIdx::HealthPotion, 5);
-	MyPlayer->GetItem(ItemIdx::ManaPotion, 5);
-	cout << "* 포션이 지급되었습니다. (HP, MP 포션 각 5개)" << '\n';
-	cout << "* 운영자의 변덕으로 체력과 MP가 절반으로 시작됩니다!" << '\n';
-	MyPlayer->HalfHp();
-	MyPlayer->HalfMp();
-	cout << "현재 HP, MP : " << MyPlayer->GetNowHp() << ", " << MyPlayer->GetNowMp() << '\n';
 }
 
 void MainGame::PlayGame()
@@ -77,14 +75,10 @@ void MainGame::PlayGame()
 
 	cout << "=============================================" << '\n';
 	cout << "<스탯 관리 시스템>" << '\n';
-	cout << "1. HP 회복" << '\n';
-	cout << "2. MP 회복" << '\n';
-	cout << "3. HP 강화" << '\n';
-	cout << "4. MP 강화" << '\n';
-	cout << "5. 공격 스킬 사용" << '\n';
-	cout << "6. 필살기 사용" << '\n';
-	cout << "7. 나가기" << '\n';
-	cout << "8. 테스터의 공격" << '\n';
+	cout << "1. 공격 하기" << '\n';
+	cout << "2. 현재 스탯 상태 출력" << '\n';
+	cout << "3. 몬스터의 공격" << '\n';
+	cout << "4. 나가기" << '\n';
 
 	int num = 0;
 	bool bGameEnd = false;
@@ -97,75 +91,35 @@ void MainGame::PlayGame()
 		{
 		case 1:
 		{
-			if (MyPlayer->GetItemCount(ItemIdx::HealthPotion) <= 0)
+			MyPlayer->Attack(MyMonster, SkillIdx::BaseAttack);
+			if (MyMonster->IsDead())
 			{
-				cout << "포션이 부족합니다" << '\n';
-			}
-			else if(MyPlayer->FullHp() == false)
-			{
-				MyPlayer->UseItem(ItemIdx::HealthPotion);
-				cout << "현재 HP : " << MyPlayer->GetNowHp() << '\n';
-				cout << "남은 HP 포션 수 : " << MyPlayer->GetItemCount(ItemIdx::HealthPotion) << '\n';
-			}
-			else
-			{
-				cout << "HP가 가득차있습니다!"<< '\n';
-				cout << "현재 HP : " << MyPlayer->GetNowHp() << '\n';
+				cout << "승리하였습니다!!!" << '\n';
+				cout << "당신의 남은 체력 : " << MyPlayer->GetNowHp() << '\n';
+				bGameEnd = true;
 			}
 		}
 		break;
 		case 2:
 		{
-			if (MyPlayer->GetItemCount(ItemIdx::ManaPotion) <= 0)
-			{
-				cout << "포션이 부족합니다" << '\n';
-			}
-			else if(MyPlayer->FullMp() == false)
-			{
-				MyPlayer->UseItem(ItemIdx::ManaPotion);
-				cout << "현재 MP : " << MyPlayer->GetNowMp() << '\n';
-				cout << "남은 MP 포션 수 : " << MyPlayer->GetItemCount(ItemIdx::ManaPotion) << '\n';
-			}
-			else
-			{
-				cout << "MP가 가득차있습니다!" << '\n';
-				cout << "현재 MP : " << MyPlayer->GetNowMp() << '\n';
-			}
+			MyPlayer->PrintPlayerStatus();
 		}
 		break;
 		case 3:
 		{
-			MyPlayer->BoostDoubleMaxHp();
-			cout << "현재 HP : " << MyPlayer->GetNowHp() << '\n';
+			MyMonster->Attack(MyPlayer, SkillIdx::BaseAttack);
+			if (MyPlayer->IsDead())
+			{
+				cout << "패배하였습니다..." << '\n';
+				cout << "몬스터의 남은 체력 : " << MyMonster->GetNowHp() << '\n';
+				bGameEnd = true;
+			}
 		}
 		break;
 		case 4:
 		{
-			MyPlayer->BoostDoubleMaxMp();
-			cout << "현재 MP : " << MyPlayer->GetNowMp() << '\n';
-		}
-		break;
-		case 5:
-		{
-			MyPlayer->Attack(tester, SkillIdx::BaseAttack);
-		}
-		break;
-		case 6:
-		{
-			MyPlayer->Attack(tester, SkillIdx::LethalAttack);
-		}
-		break;
-		case 7:
-		{
 			cout << "프로그램을 종료합니다." << '\n';
 			bGameEnd = true;
-		}
-		break;
-		case 8:
-		{
-			tester->Attack(MyPlayer, SkillIdx::BaseAttack);
-			cout << "Tester가 당신을 떼립니다!" << '\n';
-			cout << "당신의 HP : " << MyPlayer->GetNowHp() << '\n';
 		}
 		break;
 		default:
--- Task2/MainGame.h
+++ Task2/MainGame.h
@@ -14,6 +14,6 @@ protected:
 	void GameSetting();
 protected:
 	class Player* MyPlayer;
-	class Player* tester;
+	class Monster* MyMonster;
 };
 
